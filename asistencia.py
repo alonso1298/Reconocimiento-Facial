6491f146f31adef75f2f3f69ea7fdd38c5c38e95
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-06-18 15:50:37 -0600

    Se agreaga nombre a la foto

--- asistencia.py
+++ asistencia.py
@@ -1,7 +1,6 @@
 import cv2
 import face_recognition as fr
 import os
-
 import numpy
 
 # Crear base de datos 
@@ -66,4 +65,16 @@ else:
         if distancias[indice_coincidencia] > 0.6:
             print("No coincide co ninguno de nuestros empleados")
         else:
-            print("Beinvenido al trabajo")
+            # Buscar el nombre del empleado encontrado
+            nombre = nombres_empleados[indice_coincidencia]
+            
+            y1, x2, y2, x1 = caraubic
+            cv2.rectangle(imagen, (x1, y1), (x2, y2), (0, 255, 0), 2 )
+            cv2.rectangle(imagen, (x1, y2 -35), (x2, y2), (0, 255, 0), cv2.FILLED )
+            cv2.putText(imagen, nombre, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            
+            # Mostrar la imagen obtenida
+            cv2.imshow('Imagen Web', imagen)
+            
+            # Mantener la ventana abierta
+            cv2.waitKey(0)
