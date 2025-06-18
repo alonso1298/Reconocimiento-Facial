e82bc5a5fbd2b26d8f7b29c8bd9082cb8ef8dc0a
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-06-18 12:38:55 -0600

    Se crea funcion para codificar las imagenes

--- asistencia.py
+++ asistencia.py
@@ -14,3 +14,25 @@ for nombre in lista_empleados:
     nombres_empleados.append(os.path.splitext(nombre)[0])
     
 print(nombres_empleados)
+
+# Codificar imagenes
+def codificar(imagenes):
+    
+    # Crear una lista nueva
+    lista_codificada = []
+    
+    # Pasar todas las imagenes a rgb
+    for imagen in imagenes:
+        imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+        
+        # Codificar
+        codificado = fr.face_encodings(imagen[0])
+        
+        # Agregar a la lista
+        lista_codificada.append(codificado)
+        
+    # Devolver lista codificada    
+    return lista_codificada
+
+lista_empleados_codificada = codificar(mis_imagenes)
+print(len(lista_empleados_codificada))
