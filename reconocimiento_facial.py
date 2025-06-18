de70806d02625d837781b6a33f6864b68c6032f6
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-06-18 08:50:08 -0600

    Se raliza la comparacion

--- reconocimiento_facial.py
+++ reconocimiento_facial.py
@@ -13,12 +13,26 @@ foto_prueba = cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
 lugar_cara_A = fr.face_locations(foto_control)[0]
 cara_codificada_A = fr.face_encodings(foto_control)[0]
 
+lugar_cara_B = fr.face_locations(foto_prueba)[0]
+cara_codificada_B = fr.face_encodings(foto_prueba)[0]
+
 # mostrar rectangulo
 cv2.rectangle(foto_control,
               (lugar_cara_A[3], lugar_cara_A[0]),
               (lugar_cara_A[1], lugar_cara_A[2]),
               (0, 255 ,0),
               2)
+
+cv2.rectangle(foto_prueba,
+              (lugar_cara_B[3], lugar_cara_B[0]),
+              (lugar_cara_B[1], lugar_cara_B[2]),
+              (0, 255 ,0),
+              2)
+
+# Realizar comparación 
+resultado = fr.compare_faces([cara_codificada_A], cara_codificada_B)
+
+print(resultado)
 
 # Mostrar imagenes
 cv2.imshow('Foto Control', foto_control)
