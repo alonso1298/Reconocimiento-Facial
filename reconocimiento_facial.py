e5f841090fcbc918d33934fc5da0a310b1f7cab9
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-06-18 08:31:14 -0600

    Se cargan las imagenes

--- reconocimiento_facial.py
+++ reconocimiento_facial.py
@@ -1,9 +1,17 @@
-from cv2 import cv2
+import cv2
 import face_recognition as fr
 
 # Cargar Imagenes
-
-foto_control = fr.load_image_file('FotoA.jpeg')
+foto_control = fr.load_image_file('FotoA.jpg')
 foto_prueba = fr.load_image_file('FotoB.jpg')
 
+# Pasar imagenes a RGB
 foto_control = cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
+foto_prueba = cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
+
+# Mostrar imagenes
+cv2.imshow('Foto Control', foto_control)
+cv2.imshow('Foto Prueba', foto_prueba)
+
+# Mantener el programa abierto 
+cv2.waitKey(0)
