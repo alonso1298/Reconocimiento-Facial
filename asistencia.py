2d871d84d11145ab06a34307b09d404051e80e46
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-06-18 12:29:59 -0600

    Se agrega loop para guardar a los empleados

--- asistencia.py
+++ asistencia.py
@@ -2,3 +2,15 @@ import cv2
 import face_recognition as fr
 import os
 
+# Crear base de datos 
+ruta = 'Empleados'
+mis_imagenes = []
+nombres_empleados = []
+lista_empleados = os.listdir(ruta)
+
+for nombre in lista_empleados:
+    imagen_actual = cv2.imread(f'{ruta}\{nombre}')
+    mis_imagenes.append(imagen_actual)
+    nombres_empleados.append(os.path.splitext(nombre)[0])
+    
+print(nombres_empleados)
